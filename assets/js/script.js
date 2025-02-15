84e0192ed0c959dd427b6b7533f2af855f05d585
GitHub AxdeKlerk <axdeklerk@gmail.com>
2025-02-15 17:37:07 +0000

    feat: added js to stop btns scrolling on play page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
 document.getElementById("playBtn").addEventListener("click", function() {
     window.location.href = "play.html";
 });
+
+document.querySelectorAll(".btn-play").forEach(button => {
+    button.addEventListener("click", function(event) {
+        event.preventDefault(); // Stops default behavior
+    });
+});
